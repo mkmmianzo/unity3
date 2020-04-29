4094771441b68ee54d12db26d55900b060224a22
Maddie Mianzo <mmianzo@andrew.cmu.edu>
2020-04-29 10:53:36 -0400

    webs and working library puzzle

---
+++ FinalProject/Assets/Scripts/CountObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountObjects : MonoBehaviour
+{
+    public string nextLevel;
+    public GameObject objToDestroy;
+    GameObject objUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objUI = GameObject.Find("ObjectNum");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objUI.GetComponent<Text>().text = ObjectsToCollect.objects.ToString();
+        if(ObjectsToCollect.objects == 0){
+            Destroy(objToDestroy);
+            objUI.GetComponent<Text>().text = "All books collected.";
+        }
+        
+    }
+}
---
+++ FinalProject/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+	private float intensityDecreasePerSec = 0.1f; // intensity goes down by 1.0 every 5 seconds
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	GetComponent<Light>().intensity -= Time.deltaTime * intensityDecreasePerSec;
+    }
+}
